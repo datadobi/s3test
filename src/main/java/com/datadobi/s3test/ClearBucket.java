0721dddce5242a9032f81ed1c912ae49869127a0
Pepijn Van Eeckhoudt <pepijn.vaneeckhoudt@datadobi.com>
2026-02-09 18:58:10 +0100

    Add bucket cleanup utility

---
+++ src/main/java/com/datadobi/s3test/ClearBucket.java
@@ -0,0 +1,67 @@
+/*
+ *
+ *  Copyright 2025 Datadobi
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package com.datadobi.s3test;
+
+import com.datadobi.s3test.s3.S3;
+import com.datadobi.s3test.s3.S3TestBase;
+import com.datadobi.s3test.s3.ServiceDefinition;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
+import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
+import org.junit.runner.Description;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.manipulation.Filter;
+import org.junit.runner.manipulation.NoTestsRemainException;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class ClearBucket {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.err.println("Usage: ClearBucket S3_URI");
+            System.exit(1);
+        }
+
+        var target = ServiceDefinition.fromURI(args[0]);
+
+        if (!target.createBucket()) {
+            S3.deleteBucket(S3.createClient(target), target.bucket());
+            System.out.println("Bucket deleted");
+        } else {
+            System.err.println("Bucket not specified in URI");
+            System.exit(1);
+        }
+    }
+}
